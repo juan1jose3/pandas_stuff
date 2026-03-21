83d0ca24e687f77e1d75ae1ce3d521111b2cd086
juan1jose3 <moralesbenitezjuanjose@gmail.com>
2026-03-21 13:09:06 -0500

    fixed

--- TallerPythonPandas/functions/get_json.py
+++ TallerPythonPandas/functions/get_json.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
-url = "https://restcountries.com/v3.1/all?fields=name,capital,currencies"
+url = "https://pokeapi.co/api/v2/pokemon?limit=100"
 response = requests.get(url).json()
 print(json.dumps(response,ensure_ascii=False))
+
